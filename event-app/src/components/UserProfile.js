baa13d5a50b45a220f52cf4c3bf842ac9cad02e3
Sophia <82449876+SophiaSaks@users.noreply.github.com>
2022-11-24 16:31:09 +0100

    more css

--- event-app/src/components/UserProfile.js
+++ event-app/src/components/UserProfile.js
@@ -1,4 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import './UserProfile.css';
+import { Link } from 'react-router-dom'
+import LogoutButton from './auth/Logout';
 
 const UserProfile = () => {
   const { user } = useAuth0();
@@ -6,10 +9,11 @@ const UserProfile = () => {
 
 
   return (
-    <div>
-
-      <h1>User Profile</h1>
-      <img src={user.picture} alt='Profile Pic'/>
+    <div className="userProfile">
+            <Link className="addEvent__listLink" to='/'><span class="material-symbols-outlined back-icon">arrow_back_ios_new</span></Link>
+      <h1 className="userProfile__title">User Profile</h1>
+      <img className="userProfile__profile" src={user.picture} alt='Profile Pic'/>
+      <LogoutButton className="userProfile__logOutBtn" />
 
     </div>
   )
