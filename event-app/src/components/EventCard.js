dcee5af0ad3afaef82def88d4b77b85b44ffedd0
laithhilal <112702178+laithhilal@users.noreply.github.com>
2022-11-29 10:59:41 +0100

    Update EventCard.js

--- event-app/src/components/EventCard.js
+++ event-app/src/components/EventCard.js
@@ -3,8 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import axios from 'axios'
 
 const EventCard = ({ party, partyState, setPartyState, yourParties, setYourParties, showDelBtn }) => {
-  const { user, isAuthenticated } = useAuth0();
-  const { title, location, date, userID } = party
+  const { title, location, date } = party
 
   const deleteThis = () => {
     axios.delete(`http://localhost:9000/events/${party._id}`)
