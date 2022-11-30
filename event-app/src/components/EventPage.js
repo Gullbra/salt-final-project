235759a09395cd568f581a8720c45d0c87dfd275
Thor Brøndum-Eklund <thor.bde@gmail.com>
2022-11-30 11:29:17 +0100

    edit wip

--- event-app/src/components/EventPage.js
+++ event-app/src/components/EventPage.js
@@ -1,24 +1,34 @@
+import './EventPage.css'
+import { DateTime } from 'luxon'
+import { Link } from 'react-router-dom'
+import { useAuth0 } from "@auth0/auth0-react";
 
 const EventPage = ({ party }) => {
-  console.log(`hey id: ${party._id}`)
-  return (
-    <div>
-      <h1>TEST id: </h1>
-      <p>
-        sadlj
-        sacd
-        srfsaf
-        asfr
-        srrdrv
-        drvar
-        arvrev
-        arvrdav
-        arvadva
-        dtvdsvd
-        darvsd
-      </p>
+  const { user } = useAuth0()
 
-    </div>
+  const formatedDate = DateTime.fromISO(party.date)
+
+  const editFunc = () => {
+
+  }
+
+  return (
+    <section className="EventPage">
+      <Link className="userProfile__listLink" to='/'>
+        <span className="material-symbols-outlined back-icon">arrow_back_ios_new</span>
+      </Link>
+
+      <h2 className="EventPage__title">{party.title}</h2>
+      <p>{party.desc}</p>
+      <ul>
+        <li className='EventPage__location'><span>Location:</span> {party.location}</li>
+        <li className='EventPage__date'><span>Date:</span> {formatedDate.toLocaleString(DateTime.DATE_MED)}</li>
+        <li className='EventPage__time'><span>Time:</span> {formatedDate.toLocaleString(DateTime.TIME_24_SIMPLE)}</li>
+      </ul>
+
+      <button type='button' onClick={editFunc}>button</button>
+
+    </section>
   )
 }
 
--- event-app/src/components/Search.js
+++ event-app/src/components/Search.js
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react'
 import axios from 'axios';
-import './Search.css'
 
 const Search = ({setPartyState}) => {
 //   const titleField = useRef()
